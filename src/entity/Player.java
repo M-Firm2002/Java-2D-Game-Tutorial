fc1c784265b86bc50f201ba3b652e8f77f65ca7f
Michael Firman <126096960+M-Firm2002@users.noreply.github.com>
2024-02-22 15:47:02 +0000

    Add files via upload

---
+++ src/entity/Player.java
@@ -0,0 +1,119 @@
+package entity;
+
+import main.KeyHandler;
+import main.GamePanel;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Player extends Entity {
+    GamePanel gp;
+    KeyHandler kHandler;
+
+    public Player(GamePanel gp, KeyHandler kHandler) {
+        this.gp = gp;
+        this.kHandler = kHandler;
+
+        setDefaultValues();
+        getPlayerImage();
+    }
+
+    public void setDefaultValues() {
+        x = 100;
+        y = 100;
+        speed = 4;
+        direction = "down";
+    }
+
+    public void getPlayerImage() {
+        try {
+            up1 = ImageIO.read(new File("res/player/boy_up_1.png"));
+            up2 = ImageIO.read(new File("res/player/boy_up_2.png"));
+            down1 = ImageIO.read(new File("res/player/boy_down_1.png"));
+            down2 = ImageIO.read(new File("res/player/boy_down_2.png"));
+            right1 = ImageIO.read(new File("res/player/boy_right_1.png"));
+            right2 = ImageIO.read(new File("res/player/boy_right_2.png"));
+            left1 = ImageIO.read(new File("res/player/boy_left_1.png"));
+            left2 = ImageIO.read(new File("res/player/boy_left_2.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update() {
+        if (kHandler.upPressed || kHandler.downPressed || kHandler.leftPressed || kHandler.rightPressed) {
+            if (kHandler.upPressed) {
+                direction = "up";
+                y -= speed;
+            }
+            else if (kHandler.downPressed) {
+                direction = "down";
+                y += speed;
+            }
+            else if (kHandler.leftPressed) {
+                direction = "left";
+                x -= speed;
+            }
+            else if (kHandler.rightPressed) {
+                direction = "right";
+                x += speed;
+            }
+
+            spriteCounter++;
+            if (spriteCounter > 12) {
+                if (spriteNum == 1) {
+                    spriteNum = 2;
+                }
+                else if (spriteNum == 2) {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
+        }
+    }
+
+    public void draw(Graphics2D g2) {
+        BufferedImage image = null;
+
+        switch (direction) {
+            case "up":
+                if (spriteNum == 1) {
+                    image = up1;
+                }
+                if (spriteNum == 2) {
+                    image = up2;
+                }
+                break;
+            case "down":
+                if (spriteNum == 1) {
+                    image = down1;
+                }
+                if (spriteNum == 2) {
+                    image = down2;
+                }
+                break;
+            case "left":
+                if (spriteNum == 1) {
+                    image = left1;
+                }
+                if (spriteNum == 2) {
+                    image = left2;
+                }
+                break;
+            case "right":
+                if (spriteNum == 1) {
+                    image = right1;
+                }
+                if (spriteNum == 2) {
+                    image = right2;
+                }
+                break;
+        }
+        
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+    }
+}
